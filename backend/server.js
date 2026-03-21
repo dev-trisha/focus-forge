b5db8bf86b4bd85b5a31ff90e852088a1c56500a
dev-trisha <chaturveditrisha2904@gmail.com>
2026-03-21 19:06:46 +0530

    Add files via upload

---
+++ backend/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let otpStore = {};
+
+function generateOTP() {
+  return Math.floor(100000 + Math.random() * 900000);
+}
+
+// Send OTP
+app.post("/send-otp", async (req, res) => {
+  const { email } = req.body;
+
+  const otp = generateOTP();
+  otpStore[email] = otp;
+
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "yashichaturvedi2904@gmail.com",
+        pass: "xbjpidaiyykcrtsw"
+      }
+    });
+
+    await transporter.sendMail({
+      from: "FocusForge",
+      to: email,
+      subject: "Your FocusForge OTP",
+      text: `Your OTP is ${otp}`
+    });
+
+    res.json({ success: true });
+  } catch (err) {
+    console.log(err);
+    res.json({ success: false });
+  }
+});
+
+// Verify OTP
+app.post("/verify-otp", (req, res) => {
+  const { email, otp } = req.body;
+
+  if (otpStore[email] == otp) {
+    return res.json({ verified: true });
+  }
+
+  res.json({ verified: false });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
+
+app.get("/test", async (req, res) => {
+
+  const otp = 123456;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "yashichaturvedi2904@gmail.com",
+      pass: "xbjpidaiyykcrtsw"
+    }
+  });
+
+  await transporter.sendMail({
+    from: "FocusForge",
+    to: "yashichaturvedi2904@gmail.com",
+    subject: "Test OTP",
+    text: `Your OTP is ${otp}`
+  });
+
+  res.send("Email sent!");
+});
